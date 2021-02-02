7c8b45d7f7fc39eee6a08d2d87e48849b30c2b82
jakesza <41946773+jakesza@users.noreply.github.com>
2021-02-02 13:21:41 +0200

    Update esp8266-fastled-iot-webserver.ino

--- esp8266-fastled-iot-webserver.ino
+++ esp8266-fastled-iot-webserver.ino
@@ -55,8 +55,8 @@ extern "C" {
 
 /*######################## MAIN CONFIG ########################*/
 #define LED_TYPE            WS2812B                     // You might also use a WS2811 or any other strip that is Fastled compatible 
-#define DATA_PIN            D3                          // Be aware: the pin mapping might be different on boards like the NodeMCU
-//#define CLK_PIN             D5                        // Only required when using 4-pin SPI-based LEDs
+#define DATA_PIN            D4                          // Be aware: the pin mapping might be different on boards like the NodeMCU
+//#define CLK_PIN           D5                          // Only required when using 4-pin SPI-based LEDs
 #define CORRECTION          UncorrectedColor            // If colors are weird use TypicalLEDStrip
 #define COLOR_ORDER         GRB                         // Change this if colors are swapped (in my case, red was swapped with green)
 #define MILLI_AMPS          10000                       // IMPORTANT: set the max milli-Amps of your power supply (4A = 4000mA)
@@ -64,9 +64,9 @@ extern "C" {
 
 #define LED_DEBUG 0                     // enable debug messages on serial console, set to 0 to disable debugging
 
-#define DEFAULT_HOSTNAME "LEDs"         // Name that appears in your network, don't use whitespaces, use "-" instead
+#define DEFAULT_HOSTNAME "Nanoleaf-1"         // Name that appears in your network, don't use whitespaces, use "-" instead
 
-#define LED_DEVICE_TYPE 0               // The following types are available
+#define LED_DEVICE_TYPE 4               // The following types are available
 
 /*
     0: Generic LED-Strip: a regular LED-Strip without any special arrangement (and Infinity Mirror + Bottle Lighting Pad)
@@ -143,8 +143,8 @@ extern "C" {
 //---------------------------------------------------------------------------------------------------------//
 // Feature Configuration: Enabled by removing the "//" in front of the define statements
 //---------------------------------------------------------------------------------------------------------//
-    //#define ENABLE_OTA_SUPPORT                // requires ArduinoOTA - library, not working on esp's with 1MB memory (esp-01, Wemos D1 lite ...)
-        //#define OTA_PASSWORD "passwd123"      //  password that is required to update the esp's firmware wireless
+    #define ENABLE_OTA_SUPPORT                // requires ArduinoOTA - library, not working on esp's with 1MB memory (esp-01, Wemos D1 lite ...)
+      #define OTA_PASSWORD "passwd123"        //  password that is required to update the esp's firmware wireless
 
     #define ENABLE_MULTICAST_DNS              // allows to access the UI via "http://<HOSTNAME>.local/", implemented by GitHub/WarDrake
 
